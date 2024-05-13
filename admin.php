26884de3e46632ce62f5df2a95296267a21f215e
josebaqueror <167152044+josebaqueror@users.noreply.github.com>
2024-05-13 17:13:55 -0500

    actualizacion

--- admin.php
+++ admin.php
@@ -4,20 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bienvenido Administrador</title>
-  <link rel="icon" type="image/icon" href="images/logo.webp">
+  <link rel="icon" type="image/icon" href="images/logo.webp"> 
   <link rel="stylesheet" href="styleAdmin.css">
-
 </head>
 <body>
+
+    
   <header>
     <h1>Bienvenido Administrador</h1>
-    <a href="login.html" style="text-decoration: none; color: #fff; font-weight: 600;" ><img src="images/usuario.png" height="60px"><br>Salir del administrador</a>
-  </header>
+    <img src="images/logo.webp" width="100" height="100"/>
   <div class="container">
+  
     <a href="editar_productos.php" class="button">Editar Producto</a>
     <a href="Crear_producto.html" class="button">Agregar Productos</a>
     <a href="ventas_realizadas.php" class="button">Ventas Realizadas</a>
     <a href="listar_productos.php" class="button">Ver Productos</a>
+    <a href="login.html" class="button">Salir del administrador</a>
   </div>
+  
 </body>
 </html>
